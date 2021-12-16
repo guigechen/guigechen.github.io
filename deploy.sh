4be73fc128d95d2e15c76233ae48b8dd8ca94974
chen_shenggui <1260336803@qq.com>
2021-12-16 18:05:20 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ cd dist
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:guigechen/guigechen.github.io.git master
+git push git@github.com/guigechen/guigechen.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:guigechen/guigechen.git master:gh-pages
+ git push git@github.com/guigechen/guigechen.github.io.git master:gh-pages
+            
 
 cd -
